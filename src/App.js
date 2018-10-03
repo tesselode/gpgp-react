6ad8ac4d0c224a437180f63f8d2aa8a234b45fa1
Andrew Minnich <aminnich3@gmail.com>
2018-10-03 01:48:00 -0400

    getting a basic tabbed interface going

--- src/App.js
+++ src/App.js
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Button, Nav, NavItem, NavLink, TabContent, TabPane } from 'reactstrap';
+import Octicon, { Plus } from '@githubprimer/octicons-react';
 import LevelEditor from './level/LevelEditor';
 import ProjectEditor from './project/ProjectEditor';
 
-const App = () => <ProjectEditor />
+export default class App extends Component {
+	constructor(props) {
+		super(props);
 
-export default App;
+		let projectEditorTab = {title: 'New project'};
+		projectEditorTab.content = <ProjectEditor
+			onChangeTabTitle={(title) => {
+				projectEditorTab.title = title;
+			}}
+		/>
+
+		let levelEditorTab = {title: 'New level'};
+		levelEditorTab.content = <LevelEditor
+			onChangeTabTitle={(title) => {
+				levelEditorTab.title = title;
+			}}
+		/>
+
+		this.state = {
+			tabs: [projectEditorTab, levelEditorTab],
+			selectedTabIndex: 0,
+		};
+	}
+
+	render() {
+		return <div>
+			<Nav tabs>
+				{this.state.tabs.map((tab, i) =>
+					<NavItem key={i}>
+						<NavLink
+							active={this.state.selectedTabIndex === i}
+							onClick={() => this.setState({selectedTabIndex: i})}
+						>
+							{tab.title}
+						</NavLink>
+					</NavItem>
+				)}
+				<NavItem>
+					<Button
+						color='link'
+					>
+						<Octicon icon={Plus} />
+					</Button>
+				</NavItem>
+			</Nav>
+			<TabContent activeTab={this.state.selectedTabIndex}>
+				{this.state.tabs.map((tab, i) =>
+					<TabPane key={i} tabId={i}>
+						{tab.content}
+					</TabPane>
+				)}
+			</TabContent>
+		</div>
+	}
+}
