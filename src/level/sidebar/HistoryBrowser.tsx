011c838a8b2ca07be596724b88a7e626db15b968
Andrew Minnich <aminnich3@gmail.com>
2018-10-07 20:28:50 -0400

    reverse history browser order

--- src/level/sidebar/HistoryBrowser.tsx
+++ src/level/sidebar/HistoryBrowser.tsx
@@ -8,18 +8,22 @@ export interface Props {
 	historyManager: HistoryManager<Level>;
 }
 
-export default (props: Props) =>
-	<SidebarSection
+export default (props: Props) => {
+	let items: Array<JSX.Element> = [];
+	for (let i = props.historyManager.steps.length - 1; i >= 0; i--) {
+		const step = props.historyManager.steps[i];
+		items.push(<ListGroupItem
+			active={props.historyManager.position === i}
+		>
+			{step.description}
+		</ListGroupItem>)
+	}
+	return <SidebarSection
 		name='History'
 		flush
 	>
 		<ListGroup flush>
-			{props.historyManager.steps.map((step, i) => 
-				<ListGroupItem
-					active={props.historyManager.position === i}
-				>
-					{step.description}
-				</ListGroupItem>
-			)}
+			{items}
 		</ListGroup>
-	</SidebarSection>
+	</SidebarSection>;
+}
