9f5a5dcd3bb83ec3bf29b56347c30b5353a12438
Andrew Minnich <aminnich3@gmail.com>
2018-10-08 21:54:25 -0400

    history list interface

---
+++ src/data/HistoryList.ts
@@ -0,0 +1,32 @@
+export interface HistoryStep<T> {
+	data: T;
+	description: string;
+}
+
+export default interface HistoryList<T> {
+	steps: Array<HistoryStep<T>>;
+	position: number;
+}
+
+export function addHistory<T>(
+	historyList: HistoryList<T>,
+	f: (T) => string,
+	continuedAction = false,
+): HistoryList<T> {
+	let newHistory: HistoryList<T> = JSON.parse(JSON.stringify(historyList));
+	newHistory.steps = newHistory.steps.slice(0, newHistory.position + 1);
+	let newData: T = JSON.parse(JSON.stringify(newHistory.steps[newHistory.steps.length - 1].data));
+	let description = f(newData);
+	let newStep = {data: newData, description: description}
+	if (continuedAction)
+		newHistory.steps[newHistory.steps.length - 1] = newStep;
+	else {
+		newHistory.steps.push(newStep);
+		newHistory.position++;
+	}
+	return newHistory;
+}
+
+export function getCurrentHistoryState<T>(historyList: HistoryList<T>): T {
+	return historyList.steps[historyList.position].data;
+}
--- src/level/LevelEditor.tsx
+++ src/level/LevelEditor.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import Level from '../data/Level';
 import { LayerType } from '../data/layer/Layer';
 import GridEditor from './GridEditor';
+import HistoryList, { addHistory, getCurrentHistoryState } from '../data/HistoryList';
 
 export interface State {
-	level: Level;
+	levelHistory: HistoryList<Level>;
 	selectedLayerIndex: number;
 }
 
@@ -12,43 +13,60 @@ export default class LevelEditor extends React.Component<{}, State> {
 	constructor(props) {
 		super(props);
 		this.state = {
-			level: {
-				project: {
-					tileSize: 16,
-				},
-				width: 16,
-				height: 9,
-				layers: [
+			levelHistory: {
+				position: 0,
+				steps: [
 					{
-						name: 'Geometry',
-						type: LayerType.Geometry,
-						tiles: [],
-					}
-				]
+						description: 'New level',
+						data: {
+							project: {
+								tileSize: 16,
+							},
+							width: 16,
+							height: 9,
+							layers: [
+								{
+									name: 'Geometry',
+									type: LayerType.Geometry,
+									tiles: [],
+								},
+							],
+						},
+					},
+				],
 			},
 			selectedLayerIndex: 0,
 		}
 	}
 
 	onPlace(x: number, y: number) {
-		let level: Level = JSON.parse(JSON.stringify(this.state.level));
-		let layer = level.layers[this.state.selectedLayerIndex];
-		layer.tiles.push({x: x, y: y});
-		this.setState({level: level});
+		this.setState({
+			levelHistory: addHistory(this.state.levelHistory, level => {
+				let layer = level.layers[this.state.selectedLayerIndex];
+				layer.tiles.push({x: x, y: y});
+				return 'Place tiles';
+			})
+		})
 	}
 
 	onRemove(x: number, y: number) {
-		let level: Level = JSON.parse(JSON.stringify(this.state.level));
-		let layer = level.layers[this.state.selectedLayerIndex];
-		layer.tiles = layer.tiles.filter(tile => !(tile.x === x && tile.y === y));
-		this.setState({level: level});
+		this.setState({
+			levelHistory: addHistory(this.state.levelHistory, level => {
+				let layer = level.layers[this.state.selectedLayerIndex];
+				layer.tiles = layer.tiles.filter(tile => !(tile.x === x && tile.y === y));
+				return 'Remove tiles';
+			})
+		})
 	}
 
 	render() {
-		return <GridEditor
-			level={this.state.level}
-			onPlace={this.onPlace.bind(this)}
-			onRemove={this.onRemove.bind(this)}
-		/>
+		return <div>
+			<GridEditor
+				level={getCurrentHistoryState(this.state.levelHistory)}
+				onPlace={this.onPlace.bind(this)}
+				onRemove={this.onRemove.bind(this)}
+			/>
+			{this.state.levelHistory.position}
+		</div>;
 	}
 }
