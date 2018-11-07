5f66d15256fe3b9344c88434a2bbf05116267d39
Andrew Minnich <aminnich3@gmail.com>
2018-11-06 19:52:56 -0500

    fix error on adding entity layer when project has no entities

--- src/ui/cursor/entity-cursor.tsx
+++ src/ui/cursor/entity-cursor.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Entity from '../../data/entity';
 import Image from '../../data/image-data';
-import { isNullOrUndefined } from 'util';
 
 interface Props {
 	/** The tile size of the grid. */
@@ -16,34 +15,37 @@ interface Props {
 	images: Map<string, Image>;
 }
 
-const EntityCursor = (props: Props) => <div
-	style={{
-		opacity: .67,
-	}}
->
-	{props.entity.imagePath &&
-		props.images.get(props.entity.imagePath) &&
-		props.images.get(props.entity.imagePath).data ?
-		<img
-			src={props.images.get(props.entity.imagePath).data}
-			alt=''
-			style={{
-				position: 'absolute',
-				left: props.x * props.tileSize + 1 + 'px',
-				top: props.y * props.tileSize + 1 + 'px',
-				pointerEvents: 'none',
-			}}
-		/> : <div
-			style={{
-				position: 'absolute',
-				left: props.x * props.tileSize + 1 + 'px',
-				top: props.y * props.tileSize + 1 + 'px',
-				width: props.tileSize * props.entity.width + 'px',
-				height: props.tileSize * props.entity.height + 'px',
-				background: props.entity.color,
-			}}
-		/>
-	}
-</div>;
+const EntityCursor = (props: Props) => {
+	if (!props.entity) return <div />;
+	return <div
+		style={{
+			opacity: .67,
+		}}
+	>
+		{props.entity.imagePath &&
+			props.images.get(props.entity.imagePath) &&
+			props.images.get(props.entity.imagePath).data ?
+			<img
+				src={props.images.get(props.entity.imagePath).data}
+				alt=''
+				style={{
+					position: 'absolute',
+					left: props.x * props.tileSize + 1 + 'px',
+					top: props.y * props.tileSize + 1 + 'px',
+					pointerEvents: 'none',
+				}}
+			/> : <div
+				style={{
+					position: 'absolute',
+					left: props.x * props.tileSize + 1 + 'px',
+					top: props.y * props.tileSize + 1 + 'px',
+					width: props.tileSize * props.entity.width + 'px',
+					height: props.tileSize * props.entity.height + 'px',
+					background: props.entity.color,
+				}}
+			/>
+		}
+	</div>;
+};
 
 export default EntityCursor;
