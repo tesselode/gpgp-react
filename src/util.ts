b10ebf8dd3092316196366de1dd55c33b5f1d4ee
Andrew Minnich <aminnich3@gmail.com>
2018-11-17 04:44:01 -0500

    remove unused functions

--- src/util.ts
+++ src/util.ts
@@ -21,22 +21,6 @@ export function deepCopyObject<T>(object: T): T {
 	return JSON.parse(JSON.stringify(object));
 }
 
-export function shiftUp(array: any[], position: number): void {
-	if (position === 0) return;
-	const above = array[position - 1];
-	const current = array[position];
-	array[position - 1] = current;
-	array[position] = above;
-}
-
-export function shiftDown(array: any[], position: number): void {
-	if (position === array.length - 1) return;
-	const below = array[position + 1];
-	const current = array[position];
-	array[position + 1] = current;
-	array[position] = below;
-}
-
 let uniqueId = 0;
 
 export function getUniqueId(): number {
