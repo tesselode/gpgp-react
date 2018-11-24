6038646eb5cf4f5e9acf2e473c762bbfb08c2eca
Andrew Minnich <aminnich3@gmail.com>
2018-11-23 20:45:28 -0500

    organization

--- src/ui/project-editor/project-entities-tab.tsx
+++ src/ui/project-editor/project-entities-tab.tsx
@@ -70,97 +70,204 @@ export default class ProjectEntitiesTab extends React.Component<Props, State> {
 		});
 	}
 
+	private renderEntityList() {
+		return <ItemList
+			title='Entities'
+			selectedItemIndex={this.state.selectedEntityIndex}
+			items={this.props.project.data.entities}
+			onSelectItem={entityIndex => this.setState({selectedEntityIndex: entityIndex})}
+			onAddItem={() => this.props.setProject(this.props.project.addEntity())}
+			onRemoveItem={entityIndex => this.props.setProject(this.props.project.removeEntity(entityIndex))}
+			onMoveItemUp={entityIndex => this.props.setProject(this.props.project.moveEntityUp(entityIndex))}
+			onMoveItemDown={entityIndex => this.props.setProject(this.props.project.moveEntityDown(entityIndex))}
+			renderItem={entity => <div>
+				{
+					entity.data.imagePath && this.props.images.get(entity.data.imagePath) ?
+						<img
+							src={this.props.images.get(entity.data.imagePath).data}
+							style={{
+								width: 'auto',
+								height: '1em',
+							}}
+						/>
+						: ColorDisplay(entity.data.color)
+				}
+				&nbsp;&nbsp;
+				{entity.data.name}
+			</div>}
+		/>;
+	}
+
+	private renderParameterList() {
+		const selectedEntity = this.props.project.data.entities[this.state.selectedEntityIndex];
+		return <ItemList
+			title='Parameters'
+			addMenuItems={['Boolean']}
+			selectedItemIndex={this.state.selectedParameterIndex}
+			items={selectedEntity.data.parameters}
+			onSelectItem={parameterIndex => this.setState({selectedParameterIndex: parameterIndex})}
+			onAddItem={parameterType => {
+				switch (parameterType) {
+					case 0:
+						this.props.setProject(
+							this.props.project.setEntity(
+								this.state.selectedEntityIndex,
+								selectedEntity.addBooleanParameter(),
+							),
+						);
+						break;
+				}
+			}}
+			onRemoveItem={parameterIndex => this.props.setProject(
+				this.props.project.setEntity(
+					this.state.selectedEntityIndex,
+					selectedEntity.removeParameter(parameterIndex),
+				),
+			)}
+			onMoveItemUp={parameterIndex => this.props.setProject(
+				this.props.project.setEntity(
+					this.state.selectedEntityIndex,
+					selectedEntity.moveParameterUp(parameterIndex),
+				),
+			)}
+			onMoveItemDown={parameterIndex => this.props.setProject(
+				this.props.project.setEntity(
+					this.state.selectedEntityIndex,
+					selectedEntity.moveParameterDown(parameterIndex),
+				),
+			)}
+			renderItem={parameter => parameter.data.name + ' (' + parameter.data.type + ')'}
+		/>;
+	}
+
+	private renderSidebar() {
+		const selectedEntity = this.props.project.data.entities[this.state.selectedEntityIndex];
+		return <div>
+			<Nav tabs>
+				<NavItem>
+					<NavLink
+						active={this.state.activeTab === EntityEditorTab.Entities}
+						onClick={() => this.setState({activeTab: EntityEditorTab.Entities})}
+					>
+						Entities
+					</NavLink>
+				</NavItem>
+				{selectedEntity && <NavItem>
+					<NavLink
+						active={this.state.activeTab === EntityEditorTab.Parameters}
+						onClick={() => this.setState({activeTab: EntityEditorTab.Parameters})}
+					>
+						Parameters - {selectedEntity.data.name}
+					</NavLink>
+				</NavItem>}
+			</Nav>
+			<TabContent activeTab={this.state.activeTab}>
+				<TabPane tabId={EntityEditorTab.Entities}>
+					{this.renderEntityList()}
+				</TabPane>
+				{selectedEntity && <TabPane tabId={EntityEditorTab.Parameters}>
+					{this.renderParameterList()}
+				</TabPane>}
+			</TabContent>
+		</div>;
+	}
+
+	private renderEntityEditorForm() {
+		const selectedEntity = this.props.project.data.entities[this.state.selectedEntityIndex];
+		return <Form>
+			<FormGroup row>
+				<Label md={2}>Entity name</Label>
+				<Col md={10}>
+					<Input
+						value={selectedEntity.data.name}
+						onChange={event => this.props.setProject(this.props.project.setEntity(
+							this.state.selectedEntityIndex,
+							selectedEntity.setName(event.target.value),
+						))}
+					/>
+				</Col>
+			</FormGroup>
+			<FormGroup row>
+				<Label md={2}>Width</Label>
+				<Col md={10}>
+					<Input
+						value={selectedEntity.data.width}
+						onChange={event => {
+							const width = Number(event.target.value);
+							if (!isNaN(width) && width > 0)
+								this.props.setProject(this.props.project.setEntity(
+									this.state.selectedEntityIndex,
+									selectedEntity.setWidth(width),
+								));
+						}}
+					/>
+				</Col>
+			</FormGroup>
+			<FormGroup row>
+				<Label md={2}>Height</Label>
+				<Col md={10}>
+					<Input
+						value={selectedEntity.data.height}
+						onChange={event => {
+							const height = Number(event.target.value);
+							if (!isNaN(height) && height > 0)
+								this.props.setProject(this.props.project.setEntity(
+									this.state.selectedEntityIndex,
+									selectedEntity.setHeight(height),
+								));
+						}}
+					/>
+				</Col>
+			</FormGroup>
+			<FormGroup row>
+				<Label md={2}>Color</Label>
+				<Col md={10}>
+					<Input
+						type='color'
+						value={selectedEntity.data.color}
+						onChange={event => this.props.setProject(this.props.project.setEntity(
+							this.state.selectedEntityIndex,
+							selectedEntity.setColor(event.target.value),
+						))}
+					/>
+				</Col>
+			</FormGroup>
+			<FormGroup row>
+				<Label md={2}>Image path</Label>
+				<Col md={10}>
+					<InputGroup>
+						<InputGroupAddon addonType='prepend'>
+							<Button
+								onClick={() => this.chooseEntityImage()}
+							>
+								<Octicon icon={FileDirectory} />
+							</Button>
+						</InputGroupAddon>
+						<InputGroupAddon addonType='prepend'>
+							<Button
+								onClick={() => this.props.setProject(this.props.project.setEntity(
+									this.state.selectedEntityIndex,
+									selectedEntity.clearImagePath(),
+								))}
+							>
+								<Octicon icon={X} />
+							</Button>
+						</InputGroupAddon>
+						<Input
+							disabled
+							value={selectedEntity.data.imagePath || ''}
+						/>
+					</InputGroup>
+				</Col>
+			</FormGroup>
+		</Form>;
+	}
+
 	public render() {
 		const selectedEntity = this.props.project.data.entities[this.state.selectedEntityIndex];
 		return <Row>
 			<Col md={4}>
-				<Nav tabs>
-					<NavItem>
-						<NavLink
-							active={this.state.activeTab === EntityEditorTab.Entities}
-							onClick={() => this.setState({activeTab: EntityEditorTab.Entities})}
-						>
-							Entities
-						</NavLink>
-					</NavItem>
-					{selectedEntity && <NavItem>
-						<NavLink
-							active={this.state.activeTab === EntityEditorTab.Parameters}
-							onClick={() => this.setState({activeTab: EntityEditorTab.Parameters})}
-						>
-							Parameters - {selectedEntity.data.name}
-						</NavLink>
-					</NavItem>}
-				</Nav>
-				<TabContent activeTab={this.state.activeTab}>
-					<TabPane tabId={EntityEditorTab.Entities}>
-						<ItemList
-							title='Entities'
-							selectedItemIndex={this.state.selectedEntityIndex}
-							items={this.props.project.data.entities}
-							onSelectItem={entityIndex => this.setState({selectedEntityIndex: entityIndex})}
-							onAddItem={() => this.props.setProject(this.props.project.addEntity())}
-							onRemoveItem={entityIndex => this.props.setProject(this.props.project.removeEntity(entityIndex))}
-							onMoveItemUp={entityIndex => this.props.setProject(this.props.project.moveEntityUp(entityIndex))}
-							onMoveItemDown={entityIndex => this.props.setProject(this.props.project.moveEntityDown(entityIndex))}
-							renderItem={entity => <div>
-								{
-									entity.data.imagePath && this.props.images.get(entity.data.imagePath) ?
-										<img
-											src={this.props.images.get(entity.data.imagePath).data}
-											style={{
-												width: 'auto',
-												height: '1em',
-											}}
-										/>
-										: ColorDisplay(entity.data.color)
-								}
-								&nbsp;&nbsp;
-								{entity.data.name}
-							</div>}
-						/>
-					</TabPane>
-					{selectedEntity && <TabPane tabId={EntityEditorTab.Parameters}>
-						<ItemList
-							title='Parameters'
-							addMenuItems={['Boolean']}
-							selectedItemIndex={this.state.selectedParameterIndex}
-							items={selectedEntity.data.parameters}
-							onSelectItem={parameterIndex => this.setState({selectedParameterIndex: parameterIndex})}
-							onAddItem={parameterType => {
-								switch (parameterType) {
-									case 0:
-										this.props.setProject(
-											this.props.project.setEntity(
-												this.state.selectedEntityIndex,
-												selectedEntity.addBooleanParameter(),
-											),
-										);
-										break;
-								}
-							}}
-							onRemoveItem={parameterIndex => this.props.setProject(
-								this.props.project.setEntity(
-									this.state.selectedEntityIndex,
-									selectedEntity.removeParameter(parameterIndex),
-								),
-							)}
-							onMoveItemUp={parameterIndex => this.props.setProject(
-								this.props.project.setEntity(
-									this.state.selectedEntityIndex,
-									selectedEntity.moveParameterUp(parameterIndex),
-								),
-							)}
-							onMoveItemDown={parameterIndex => this.props.setProject(
-								this.props.project.setEntity(
-									this.state.selectedEntityIndex,
-									selectedEntity.moveParameterDown(parameterIndex),
-								),
-							)}
-							renderItem={parameter => parameter.data.name + ' (' + parameter.data.type + ')'}
-						/>
-					</TabPane>}
-				</TabContent>
+				{this.renderSidebar()}
 			</Col>
 			{selectedEntity && <Col
 				md={8}
@@ -170,93 +277,7 @@ export default class ProjectEntitiesTab extends React.Component<Props, State> {
 					overflowY: 'auto',
 				}}
 			>
-				<Form>
-					<FormGroup row>
-						<Label md={2}>Entity name</Label>
-						<Col md={10}>
-							<Input
-								value={selectedEntity.data.name}
-								onChange={event => this.props.setProject(this.props.project.setEntity(
-									this.state.selectedEntityIndex,
-									selectedEntity.setName(event.target.value),
-								))}
-							/>
-						</Col>
-					</FormGroup>
-					<FormGroup row>
-						<Label md={2}>Width</Label>
-						<Col md={10}>
-							<Input
-								value={selectedEntity.data.width}
-								onChange={event => {
-									const width = Number(event.target.value);
-									if (!isNaN(width) && width > 0)
-										this.props.setProject(this.props.project.setEntity(
-											this.state.selectedEntityIndex,
-											selectedEntity.setWidth(width),
-										));
-								}}
-							/>
-						</Col>
-					</FormGroup>
-					<FormGroup row>
-						<Label md={2}>Height</Label>
-						<Col md={10}>
-							<Input
-								value={selectedEntity.data.height}
-								onChange={event => {
-									const height = Number(event.target.value);
-									if (!isNaN(height) && height > 0)
-										this.props.setProject(this.props.project.setEntity(
-											this.state.selectedEntityIndex,
-											selectedEntity.setHeight(height),
-										));
-								}}
-							/>
-						</Col>
-					</FormGroup>
-					<FormGroup row>
-						<Label md={2}>Color</Label>
-						<Col md={10}>
-							<Input
-								type='color'
-								value={selectedEntity.data.color}
-								onChange={event => this.props.setProject(this.props.project.setEntity(
-									this.state.selectedEntityIndex,
-									selectedEntity.setColor(event.target.value),
-								))}
-							/>
-						</Col>
-					</FormGroup>
-					<FormGroup row>
-						<Label md={2}>Image path</Label>
-						<Col md={10}>
-							<InputGroup>
-								<InputGroupAddon addonType='prepend'>
-									<Button
-										onClick={() => this.chooseEntityImage()}
-									>
-										<Octicon icon={FileDirectory} />
-									</Button>
-								</InputGroupAddon>
-								<InputGroupAddon addonType='prepend'>
-									<Button
-										onClick={() => this.props.setProject(this.props.project.setEntity(
-											this.state.selectedEntityIndex,
-											selectedEntity.clearImagePath(),
-										))}
-									>
-										<Octicon icon={X} />
-									</Button>
-								</InputGroupAddon>
-								<Input
-									disabled
-									value={selectedEntity.data.imagePath || ''}
-								/>
-							</InputGroup>
-						</Col>
-					</FormGroup>
-				</Form>
+				{this.renderEntityEditorForm()}
 			</Col>}
 		</Row>;
 	}
