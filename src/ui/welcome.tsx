2a9f6a9c2629fd63c87bab432b4c4ce2ea85a800
Andrew Minnich <aminnich3@gmail.com>
2018-11-25 14:39:39 -0500

    add a pleasant animation to the welcome screen

--- src/ui/welcome.tsx
+++ src/ui/welcome.tsx
@@ -12,38 +12,42 @@ export interface Props {
 
 /** The introductory screen that is shown when no tabs are open. */
 const Welcome = (props: Props) =>
-	<Jumbotron>
-		<h1>Welcome to GPGP</h1>
-		<p>Open an existing project or level, or create a new project.</p>
-		<Row>
-			<Col md={4}>
-				<Button
-					block
-					onClick={() => props.onCreateNewProject()}
-				>
-					New project
-				</Button>
-				<br />
-				<Row>
-					<Col md={6}>
-						<Button
-							block
-							onClick={() => props.onOpenProject()}
-						>
-							Open project
-						</Button>
-					</Col>
-					<Col md={6}>
-						<Button
-							block
-							onClick={() => props.onOpenLevel()}
-						>
-							Open level
-						</Button>
-					</Col>
-				</Row>
-			</Col>
-		</Row>
+	<Jumbotron id='welcome-jumbotron'>
+		<div id='welcome-text'>
+			<h1>Welcome to GPGP</h1>
+			<p>Open an existing project or level, or create a new project.</p>
+		</div>
+		<div id='welcome-buttons'>
+			<Row>
+				<Col md={4}>
+					<Button
+						block
+						onClick={() => props.onCreateNewProject()}
+					>
+						New project
+					</Button>
+					<br />
+					<Row>
+						<Col md={6}>
+							<Button
+								block
+								onClick={() => props.onOpenProject()}
+							>
+								Open project
+							</Button>
+						</Col>
+						<Col md={6}>
+							<Button
+								block
+								onClick={() => props.onOpenLevel()}
+							>
+								Open level
+							</Button>
+						</Col>
+					</Row>
+				</Col>
+			</Row>
+		</div>
 	</Jumbotron>;
 
 export default Welcome;
