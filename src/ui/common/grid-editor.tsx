4626e5ec5196786f6e3289e0ff4fa32519dd7eec
Andrew Minnich <aminnich3@gmail.com>
2018-12-04 02:21:51 -0500

    panning

--- src/ui/common/grid-editor.tsx
+++ src/ui/common/grid-editor.tsx
@@ -9,8 +9,27 @@ interface Props {
     level: Level;
 }
 
-export default class GridEditor extends React.Component<Props> {
+interface State {
+    mouseX: number;
+    mouseY: number;
+    panX: number;
+    panY: number;
+    panning: boolean;
+}
+
+export default class GridEditor extends React.Component<Props, State> {
     private canvasRef = React.createRef<HTMLCanvasElement>();
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            mouseX: 0,
+            mouseY: 0,
+            panX: 0,
+            panY: 0,
+            panning: false,
+        };
+    }
 
     private renderGridlines(context: CanvasRenderingContext2D) {
         const tileSize = this.props.project.data.tileSize;
@@ -34,10 +53,16 @@ export default class GridEditor extends React.Component<Props> {
     }
 
     private renderCanvas() {
+        const tileSize = this.props.project.data.tileSize;
+        const width = this.props.level.data.width;
+        const height = this.props.level.data.height;
         const canvas = this.canvasRef.current;
         canvas.width = this.props.viewportWidth;
         canvas.height = this.props.viewportHeight;
         const context = canvas.getContext('2d');
+        context.translate(this.props.viewportWidth / 2, this.props.viewportHeight / 2);
+        context.translate(-(width * tileSize) / 2, -(height * tileSize) / 2);
+        context.translate(this.state.panX, this.state.panY);
         this.renderGridlines(context);
     }
 
@@ -50,6 +75,31 @@ export default class GridEditor extends React.Component<Props> {
     }
 
     public render() {
-        return <canvas ref={this.canvasRef} />;
+        return <canvas
+            ref={this.canvasRef}
+            onMouseDown={event => {
+                switch (event.button) {
+                    case 1:
+                        this.setState({panning: true});
+                }
+            }}
+            onMouseUp={event => {
+                switch (event.button) {
+                    case 1:
+                        this.setState({panning: false});
+                }
+            }}
+            onMouseMove={event => {
+                if (this.state.panning)
+                    this.setState({
+                        panX: this.state.panX + event.clientX - this.state.mouseX,
+                        panY: this.state.panY + event.clientY - this.state.mouseY,
+                    });
+                this.setState({
+                    mouseX: event.clientX,
+                    mouseY: event.clientY,
+                });
+            }}
+        />;
     }
 }
