b2b687452b39585abe691610c5d792969ef19009
Andrew Minnich <aminnich3@gmail.com>
2018-12-18 16:10:08 -0500

    grid rendering

--- src/app.tsx
+++ src/app.tsx
@@ -1,38 +1,13 @@
-import * as PIXI from 'pixi.js';
 import React from 'react';
+import Grid from './grid';
 
-export default class App extends React.Component<{}> {
-	private pixiApp = new PIXI.Application({
-		width: 500,
-		height: 500,
-		transparent: true,
-	});
-	private containerRef = React.createRef<HTMLDivElement>();
-
-	constructor(props: {}) {
-		super(props);
-	}
-
-	public componentDidMount() {
-		this.containerRef.current.appendChild(this.pixiApp.view);
-		PIXI.loader
-			.add([
-				'D:/Programming/gpgp-react/assets/seasonal platformer/fall.png',
-			])
-			.load(() => {
-				const texture = PIXI.loader.resources['D:/Programming/gpgp-react/assets/seasonal platformer/fall.png'].texture;
-				texture.frame = new PIXI.Rectangle(16, 64, 16, 16);
-				const sprite = new PIXI.Sprite(texture);
-				sprite.x = 64;
-				sprite.y = 64;
-				this.pixiApp.stage.scale = new PIXI.Point(4, 4);
-				this.pixiApp.stage.addChild(sprite);
-			});
-	}
-
+export default class App extends React.Component {
 	public render() {
-		return <div
-			ref={this.containerRef}
+		return <Grid
+			viewportWidth={window.innerWidth}
+			viewportHeight={window.innerHeight}
+			width={32}
+			height={18}
 		/>;
 	}
 }
---
+++ src/grid.tsx
@@ -0,0 +1,64 @@
+import * as PIXI from 'pixi.js';
+import React from 'react';
+
+interface Props {
+	/** The width of the space available for the grid. */
+	viewportWidth: number;
+	/** The height of the space available for the grid. */
+	viewportHeight: number;
+	/** The width of the grid (in tiles). */
+	width: number;
+	/** The height of the grid (in tiles). */
+	height: number;
+}
+
+/** An interactive grid that can display content. */
+export default class Grid extends React.Component<Props> {
+	private pixiApp: PIXI.Application;
+	private containerRef = React.createRef<HTMLDivElement>();
+	private zoom = 16;
+
+	constructor(props: Props) {
+		super(props);
+		this.pixiApp = new PIXI.Application({
+			width: props.viewportWidth,
+			height: props.viewportHeight,
+			transparent: true,
+		});
+	}
+
+	private createGridlines() {
+		const gridlines = new PIXI.Graphics();
+		gridlines.lineStyle(2 / this.zoom, 0x000000, .1);
+		for (let x = 1; x < this.props.width; x++) {
+			gridlines.moveTo(x, 0);
+			gridlines.lineTo(x, this.props.height);
+		}
+		for (let y = 1; y < this.props.height; y++) {
+			gridlines.moveTo(0, y);
+			gridlines.lineTo(this.props.width, y);
+		}
+		return gridlines;
+	}
+
+	private createBorder() {
+		const border = new PIXI.Graphics();
+		border.lineStyle(2 / this.zoom, 0x000000);
+		border.lineAlignment = 0;
+		border.drawRect(0, 0, this.props.width, this.props.height);
+		return border;
+	}
+
+	public componentDidMount() {
+		this.containerRef.current.appendChild(this.pixiApp.view);
+		this.pixiApp.stage.addChild(this.createGridlines());
+		this.pixiApp.stage.addChild(this.createBorder());
+		this.pixiApp.stage.scale = new PIXI.Point(this.zoom, this.zoom);
+	}
+
+	public render() {
+		return <div
+			ref={this.containerRef}
+		/>;
+	}
+}
