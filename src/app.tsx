dbe0b09bc3f57093661d024b040362a260b20625
Andrew Minnich <aminnich3@gmail.com>
2018-12-18 15:53:57 -0500

    i drew a tile

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import * as PIXI from 'pixi.js';
 import React from 'react';
 
-export default class App extends React.Component {
+export default class App extends React.Component<{}> {
 	private pixiApp = new PIXI.Application({
 		width: 500,
 		height: 500,
@@ -9,8 +9,25 @@ export default class App extends React.Component {
 	});
 	private containerRef = React.createRef<HTMLDivElement>();
 
+	constructor(props: {}) {
+		super(props);
+	}
+
 	public componentDidMount() {
 		this.containerRef.current.appendChild(this.pixiApp.view);
+		PIXI.loader
+			.add([
+				'D:/Programming/gpgp-react/assets/seasonal platformer/fall.png',
+			])
+			.load(() => {
+				const texture = PIXI.loader.resources['D:/Programming/gpgp-react/assets/seasonal platformer/fall.png'].texture;
+				texture.frame = new PIXI.Rectangle(16, 64, 16, 16);
+				const sprite = new PIXI.Sprite(texture);
+				sprite.x = 64;
+				sprite.y = 64;
+				this.pixiApp.stage.scale = new PIXI.Point(4, 4);
+				this.pixiApp.stage.addChild(sprite);
+			});
 	}
 
 	public render() {
